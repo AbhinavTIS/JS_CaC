f4933c3f5ee81fab0e15c3607b0bb29db5572bd4
Abhinav Mahajan <96789490+AbhinavTIS@users.noreply.github.com>
2024-01-18 20:34:34 +0000

    learn comparison opertors

---
+++ 01_basics/comparison.js
@@ -0,0 +1,14 @@
+// // console.log("2"> 1); 
+// console.log("1"==1); // conversion happens
+// console.log("1"===1); // compares data type as well
+
+
+// console.log(null==0) //f
+// console.log(null>0) //f
+// console.log(null>=0) //t
+
+// The relational operators(>< >= <=) work differently than == operator
+
+console.log(undefined==0)
+console.log(undefined>0);
+console.log(undefined>=0);
