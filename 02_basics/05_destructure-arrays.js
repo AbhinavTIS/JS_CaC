aba6849c3b57f8ad9493faa23d44f6109ee0338f
Abhinav Mahajan <96789490+AbhinavTIS@users.noreply.github.com>
2024-01-25 00:43:20 +0000

    de-structured arrays

---
+++ 02_basics/05_destructure-arrays.js
@@ -0,0 +1,7 @@
+const alphabet =['A','B','C','D'];
+const numbers =[1,2,3,4]
+
+const [first,second,,fourth]= alphabet; // skipped third element
+
+console.log(`first element in alphabet array is ${first} and last one is ${fourth} `); // no need to always access first element of array using index now
+
